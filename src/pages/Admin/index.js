c2b0d7f82135ba3f4815ce9da5a4e338ba234011
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-11-29 11:56:43 -0300

    image

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,18 +1,64 @@
 import "./painel.css";
+import { useState, useEffect } from "react";
+import {toast} from "react-toastify";
 
-export default function Painel(){
-    return(
+//importando banco de dados
+import { db } from "../../db/firebaseConection";
+import { addDoc, collection, onSnapshot, query, orderBy, doc, deleteDoc } from "firebase/firestore";
+
+export default function Painel() {
+    const [tituloAdmin, setTitulo] = useState('');
+    const [materia, setMateria] = useState('');
+    const [foto, setFoto] = useState('')
+
+
+     //função que salva as noticias
+     async function SalvaRegitro() {
+        if(tituloAdmin === '' || setTitulo === ''){
+            toast.error('Preencha todos os campos');
+            return;
+        }
+
+  
+        addDoc(collection(db, "noticias"), {
+            titulo: tituloAdmin,
+            materia: materia,
+            foto: foto,
+            created: new Date(),
+        }).then(()=>{
+            setTitulo('');
+            setMateria('');
+            setFoto('');
+            toast.success('salvo com sucesso!');
+        }).catch((err)=>{
+            toast.error('que pena não salvou !')
+        })
+       
+    }
+
+   
+    return (
         <>
-        
-        <div className="card-materia" >
-            <label>Título da publicação</label>
-            <input type={'text'}  placeholder="Título.." />
-            <label>Texto da publicação</label>
-            <textarea  className="text" />
 
-            <label>Imagem</label>
-            <input type={'file'} />
-        </div>
+            <div className="card-materia" >
+                <label>Título da publicação</label>
+                <input type={'text'} placeholder="Título.." 
+                     value={tituloAdmin}
+                     onChange={(e) => setTitulo(e.target.value)}
+                />
+                <label>Texto da publicação</label>
+                <textarea className="text" 
+                    value={materia}
+                    onChange={(e) => setMateria(e.target.value)}
+                />
+
+                <label>Imagem</label>
+                <input type={'file'} 
+                    value={foto}
+                    onChange={(e) => setFoto(e.target.value)}
+                />
+                <button className="btn-salvar" onClick={SalvaRegitro} >Salvar</button>
+            </div>
 
         </>
     )
--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,9 +1,40 @@
 import "./news.css";
+import { useState, useEffect } from "react";
+
+//importando banco de dados
+import { db } from "../../db/firebaseConection";
+import { collection, onSnapshot, query, orderBy, doc, deleteDoc } from "firebase/firestore";
 
 //importando arquivos
 import imagem from "../../images/natalSemFome.jpeg";
 
 export default function News() {
+
+     //buscando registros no firestory
+     const [links, setLikis] = useState([]);
+
+     useEffect(() => {
+ 
+         const linksRef = collection(db, "noticias");
+         const queryRef = query(linksRef, orderBy("created", "asc"));
+ 
+         const unsub = onSnapshot(queryRef, (snapshot) => {
+             let lista = [];
+             snapshot.forEach((doc) => {
+                 lista.push({
+                     id: doc.id,
+                     titulo: doc.data().titulo,
+                     materia: doc.data().materia,
+                     foto: doc.data().foto
+                 })
+             })
+ 
+             setLikis(lista.slice(0,10));
+ 
+         })
+ 
+     }, []);
+
     return (
         <section className="card-news" >
             
@@ -12,40 +43,19 @@ export default function News() {
                 <label>Natal sem Fome</label>
             </a>
 
+             {links.map((item,index)=>(
+                 <div className="card-news-block" >
 
-            <div className="card-news-block" >
+                 <div className="card-news-item" >
+                     <img src={imagem} /> <label>Titulo</label>
+                     <label>{item.materia.substring(0,50)}..</label>
+                     <a className="btn-mais" href="/ler" >Leia mais</a>
+                 </div>
+               
+             </div>
+             ))}
+           
 
-                <div className="card-news-item" >
-                    <img src={imagem} /> <label>Titulo</label>
-                    <label>Capanha natal sem fomo da paróquia de cocal</label>
-                    <a className="btn-mais" href="/ler" >Leia mais</a>
-                </div>
-                <div className="card-news-item" >
-                    <img src={imagem} /> <label>Titulo</label>
-                    <label>Capanha natal sem fomo da paróquia de cocal</label>
-                    <a className="btn-mais" href="/ler" >Leia mais</a>
-                </div>
-                <div className="card-news-item" >
-                    <img src={imagem} /> <label>Titulo</label>
-                    <label>Capanha natal sem fomo da paróquia de cocal</label>
-                    <a className="btn-mais" href="/ler" >Leia mais</a>
-                </div>
-                <div className="card-news-item" >
-                    <img src={imagem} /> <label>Titulo</label>
-                    <label>Capanha natal sem fomo da paróquia de cocal</label>
-                    <a className="btn-mais" href="/ler" >Leia mais</a>
-                </div>
-                <div className="card-news-item" >
-                    <img src={imagem} /> <label>Titulo</label>
-                    <label>Capanha natal sem fomo da paróquia de cocal</label>
-                    <a className="btn-mais" href="/ler" >Leia mais</a>
-                </div>
-
-            </div>
-
-
-
-            
 
         </section>
     )
