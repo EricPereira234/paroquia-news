aa005ab436943dcbf23cb03de51f335c8037d9ae
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-12-14 14:50:49 -0300

    teste

--- src/pages/LerMais/index.js
+++ src/pages/LerMais/index.js
@@ -4,20 +4,54 @@ import { useState, useEffect } from "react";
 
 //importando arquivos
 import Header from "../../components/Header";
+import { where } from "firebase/firestore";
+
+
+//importando banco de dados
+import { db } from "../../db/firebaseConection";
+import { collection, onSnapshot, query, orderBy, doc, deleteDoc } from "firebase/firestore";
 
 
 export default function Ler() {
 
+
+    //buscando registros no firestory
+    const [links, setLikis] = useState([]);
+
+    useEffect(() => {
+
+        const linksRef = collection(db, "noticias");
+        const queryRef = query(linksRef, orderBy("created", "asc"));
+
+        const unsub = onSnapshot(queryRef, (snapshot) => {
+            let lista = [];
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: where (doc.data()),
+                    titulo: doc.data().titulo,
+                    materia: doc.data().materia
+                })
+            })
+
+            setLikis(lista.slice(0,30));
+
+        })
+
+    }, []);
+ 
   
     return (
         <div className="card-ler">
             <Header />
+
+            {links.map((item, index) => (
          
             <div className="card-ler-body" >
-                <h3></h3>
+                <h3>{item.titulo}</h3>
                 desevolvimento da matéria
             </div>
             
+            ))}
       
         
         </div>
