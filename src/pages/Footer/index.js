388ab0b09de793ba296e193259b84f760085a497
Eric Pereira <92660469+EricPereira234@users.noreply.github.com>
2022-12-26 12:04:50 -0300

    footer

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -1,9 +1,17 @@
 import "./footer.css";
 
+//importando arquivos
+import logo from "../../images/logoEric.png";
+
 export default function Footer(){
     return(
         <footer>
-            &copy;2022 - EP
+          <label>Paróquia Nossa Senhora do Perpétuos Socorro</label>
+          <div className="card-logo-footer" >
+            Criado por - 
+            <img src={logo} /> <span>Eric Pereira</span>
+          </div>
+          
         </footer>
     )
 }
